d9b5801775b10b5f80419d126f6ad50126679b70
Yaakov Grinboim <yaakovgrinboim@gmail.com>
2026-07-09 12:41:47 +0300

    feat(db): add read and write utility functions

---
+++ week13/project_express/dbManager.js
@@ -0,0 +1,25 @@
+import fs from "fs/promises";
+
+const DB_BASE_PATH = process.env.DB_BASE_PATH || "./db";
+
+export async function readFileContent(fileName) {
+    try {
+        const filePath = `${DB_BASE_PATH}/${fileName}`
+        const fileContent = await fs.readFile(filePath, "utf-8");
+        return JSON.parse(fileContent);
+    } catch (error) {
+        console.error(`Error reading from ${fileName}:`, error.message)
+    } return [];
+};
+
+export async function writeToFile(fileName, data) {
+    try {
+        const filePath = `${DB_BASE_PATH}/${fileName}`
+        const fileContent = await fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf-8");
+        return true;
+    } catch (error) {
+        console.error(`Error writing to ${fileName}:`, error.message)
+    } return false;
+};
+
+
